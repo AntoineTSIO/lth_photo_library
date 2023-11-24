0495ed7b8f94009d3da18a9b1adb43d9f233c42b
AntoineTSIO <antoine.touzart@outlook.com>
2023-11-24 12:46:59 +0100

    feat: finish logging form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { Button, Input } from 'antd';
+import { Button, Input, Checkbox, Form } from 'antd';
 import './App.css';
 
 interface LoginFormProps {
     onLogin: (username: string, password: string) => void;
 }
+const onFinish = (values: any) => {
+    console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+};
+
+type FieldType = {
+    username?: string;
+    password?: string;
+};
 
 const App: React.FC = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -32,29 +43,42 @@ const App: React.FC = () => {
                 </div>
 
                 <div className="content">
-                    <div className="form">
-                        <label>
-                            Username:
-                            <input
-                                className="form-input"
-                                type="text"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                        </label>
-                        <br/>
-                        <label>
-                            Password:
-                            <input
-                                className="form-input"
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </label>
-                        <br/>
-                        <Button type="primary" onClick={handleLogin}>Login</Button>
-                    </div>
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item<FieldType>
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item<FieldType>
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                            <Button type="primary" htmlType="submit">
+                                Se Connecter
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+
+                <div className="content">
+                    <p>test</p>
                 </div>
 
                 <div className="footer">
